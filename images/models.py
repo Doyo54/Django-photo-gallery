8879318190f3b7d7b708dc87df1edc2830feb67b
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-25 15:06:31 +0300

    ft: Image model created

--- images/models.py
+++ images/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length =30)
+    image = models.ImageField(upload_to = 'image/',null =True)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
