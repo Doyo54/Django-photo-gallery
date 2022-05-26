48665bc26dd988b8ef4bc9e1ae66217c6a0e85f9
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-26 15:14:26 +0300

    ft: Two tables created for category and location. Both have a One to Many Relationship with the images

--- images/models.py
+++ images/models.py
@@ -1,15 +1,24 @@
+from unicodedata import category
 from django.db import models
 import datetime as dt
+
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+
+
+class Location(models.Model):
+    location = models.CharField(max_length=30)
 
 class Image(models.Model):
     name = models.CharField(max_length =30)
     image = models.ImageField(upload_to = 'image/',null =True)
     description = models.CharField(max_length=200)
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
 
     def __str__(self):
         return self.name
     
     def save_image(self):
         self.save()
-
 
