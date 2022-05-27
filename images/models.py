82a4ea38cbdc03781c86df66d6c699973b72bfe0
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-27 21:05:42 +0300

    ft: Method created to get an image and update it

--- images/models.py
+++ images/models.py
@@ -56,5 +56,9 @@ class Image(models.Model):
         image_location = Image.objects.filter(location__name=location).all()
         return image_location
 
- 
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+
 
