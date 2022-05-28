7aab0402f1c5368e3047f0c7159cb095c8c6182b
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-28 13:30:46 +0300

    ft: Search button styled and view function to get search results created

--- images/urls.py
+++ images/urls.py
@@ -9,6 +9,7 @@ urlpatterns=[
     url(r'^index',views.index,name = 'index'),
     url(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
     url(r'^category/(?P<category>\w+)/', views.image_category, name='category'),
+    url(r'^search/', views.search_results, name='search'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- images/views.py
+++ images/views.py
@@ -10,7 +10,7 @@ def home(request):
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
-    
+
 def image_location(request, location):
     images = Image.filter_by_location(location)
     print(images)
@@ -20,3 +20,14 @@ def image_category(request, category):
     images = Image.filter_by_category(category)
     print(images)
     return render(request, 'location.html', {'location_images': images})
+
+def search_results(request):
+    if 'search' in request.GET and request.GET["search"]:
+        category = request.GET.get("search")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request, 'search_results.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image category"
+        return render(request, 'search_results.html', {"message": message})
