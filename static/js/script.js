11589a1c6a01817f71893e1140fb173f7ba61c8a
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-29 20:27:41 +0300

    ft: Final touches on styling and deployment was successful

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
 copyToClipboard=(value) => {
-    navigator.clipboard.writeText(value)
+    navigator.clipboard.writeText(value);
+    alert("Url copied successfully. Paste it after the default url -https://gallery-doyo.herokuapp.com ");
           }
