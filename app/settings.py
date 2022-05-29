7945ebfc393a38af5b91448c630dd49f1b0cc78f
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-29 20:41:56 +0300

    ft: Tests for image model created

--- app/settings.py
+++ app/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 from decouple import config,Csv
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+
 # development
 if config('MODE')=="dev":
    DATABASES = {
@@ -32,15 +32,9 @@ if config('MODE')=="dev":
        
    }
 # production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,41 @@
+from unicodedata import category
 from django.test import TestCase
+from .models import Image, Category, Location
 
-# Create your tests here.
+
+class TestImage(TestCase):
+    def setUp(self):
+        self.location = Location(location='Nairobi')
+        self.location.save_location()
+
+        self.category = Category(category='Travel')
+        self.category.save_category()
+
+        self.image_test = Image(id=1, name='food', description='this is a test image', location=self.location,
+                                category=self.category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def test_update_image(self):
+        self.image_test.save_image()
+        self.image_test.update_image(self.image_test.id, 'image/test.jpg')
+        changed_img = Image.objects.filter(image='image/test.jpg')
+        self.assertTrue(len(changed_img) > 0)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+
+
